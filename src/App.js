897f4d6abe95d0fbe028bb83d3c32a41d6576d10
zubayerhimel <zhimel007@gmail.com>
2023-11-21 23:26:41 +0600

    feat: added card layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
       <span className='title'>Expense Chart</span>
       <div className='card'>
         <span className='card-title'>Expenses</span>
+        <div className='time-tab'>
+          <ul>
+            <li>1M</li>
+            <li>6M</li>
+            <li>1Y</li>
+            <li>ALL TIME</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
